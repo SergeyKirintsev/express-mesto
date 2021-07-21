bb604eed4631ba1910d9970f19c4be87afe5b2b4
SergeyKirintsev <kserg80@gmail.com>
2021-07-21 22:15:08 +0300

    создать схему user, card

---
+++ models/card.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+// Опишем схему:
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// создаём модель и экспортируем её
+module.exports = mongoose.model('card', cardSchema);
