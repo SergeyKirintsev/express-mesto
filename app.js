49a0c2ba267cdb910c67ddee9aadaad21191381a
SergeyKirintsev <kserg80@gmail.com>
2021-07-21 21:02:44 +0300

    установить mongoose

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`Server run on port ${PORT}`);
