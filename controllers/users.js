8eb7fae7ec38cb8075501b0bc14119d22cbd3301
SergeyKirintsev <kserg80@gmail.com>
2021-07-22 22:34:01 +0300

    реализовать доп роуты юзера

--- controllers/users.js
+++ controllers/users.js
@@ -21,8 +21,46 @@ const getUserById = (req, res) => {
     .catch(() => res.status(500).send({ message: 'Пользователь не найден' }));
 };
 
+const updateProfile = (req, res) => {
+  const userId = req.user._id;
+  const { name, about } = req.body;
+  console.log(userId);
+  console.log(name, about);
+
+  User.findByIdAndUpdate(
+    userId,
+    { name, about },
+    {
+      new: true, // обработчик then получит на вход обновлённую запись
+      runValidators: true, // данные будут валидированы перед изменением
+      upsert: true, // если пользователь не найден, он будет создан
+    },
+  )
+    .then((user) => res.send(user))
+    .catch(() => res.status(500).send({ message: 'Пользователь не найден' }));
+};
+
+const updateAvatar = (req, res) => {
+  const userId = req.user._id;
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(
+    userId,
+    { avatar },
+    {
+      new: true, // обработчик then получит на вход обновлённую запись
+      runValidators: true, // данные будут валидированы перед изменением
+      upsert: true, // если пользователь не найден, он будет создан
+    },
+  )
+    .then((user) => res.send(user))
+    .catch(() => res.status(500).send({ message: 'Пользователь не найден' }));
+};
+
 module.exports = {
   createUser,
   getUsers,
   getUserById,
+  updateProfile,
+  updateAvatar,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
-const { createUser, getUsers, getUserById } = require('../controllers/users');
+const {
+  createUser,
+  getUsers,
+  getUserById,
+  updateProfile,
+  updateAvatar,
+} = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUserById);
 
 router.post('/', createUser);
+
+router.patch('/me', updateProfile);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
