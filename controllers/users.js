f173b34a54d315fab49981cdff10b60516376395
SergeyKirintsev <kserg80@gmail.com>
2021-07-22 20:50:53 +0300

    создать котроллеры и роуты для пользователей

---
+++ controllers/users.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Пользователь не создан' }));
+};
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Ошибка' }));
+};
+
+const getUserById = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Пользователь не найден' }));
+};
+
+module.exports = {
+  createUser,
+  getUsers,
+  getUserById,
+};
