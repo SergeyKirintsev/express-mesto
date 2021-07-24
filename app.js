a4199f14917cf47f5c0ad95944f676fb0397b304
SergeyKirintsev <kserg80@gmail.com>
2021-07-24 15:21:35 +0300

    useUnifiedTopology: true

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 app.use((req, res, next) => {
