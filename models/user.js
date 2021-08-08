e674cc1df08f7f2b458043d534c22fae0abb3a96
SergeyKirintsev <kserg80@gmail.com>
2021-08-08 23:21:42 +0300

    убрать пароль из возвращаемых данных

--- models/user.js
+++ models/user.js
@@ -32,11 +32,13 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 8,
+    select: false,
   },
 });
 
+// eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
