969e526f83419e249d04ceeac35716812aa3a44d
SergeyKirintsev <kserg80@gmail.com>
2021-08-08 11:49:29 +0300

    Добавить email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -17,6 +17,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 // создаём модель и экспортируем её
